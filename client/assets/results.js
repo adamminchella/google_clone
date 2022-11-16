f39dde4cc33bb93e2c249ada4d413bb2ffdc84fc
Adam Minchella <adamminchella@msn.com>
2022-11-16 18:57:53 +0000

    Display results on new page

--- client/assets/results.js
+++ client/assets/results.js
@@ -1,15 +1,23 @@
-const results = localStorage.getItem("results");
-console.log(JSON.parse(results));
+let results = localStorage.getItem("results");
+results = JSON.parse(results);
 
-let resultsData = [];
 let titleData = [];
 let descriptionData = [];
 let linkData = [];
 for (let i = 0; i < 10; i++) {
-  resultsData[i] = document.getElementById(`result${i + 1}`);
-  titleData[i] = document.getElementById(`title${i + 1}`);
-  descriptionData[i] = document.getElementById(`description${i + 1}`);
-  linkData[i] = document.getElementById(`link${i + 1}`);
+  titleData[i] = document.querySelector(`.title${i + 1}`);
+  descriptionData[i] = document.querySelector(`.description${i + 1}`);
+  linkData[i] = document.querySelector(`.link${i + 1}`);
 }
 
-document.addEventListener("load", () => {});
+window.addEventListener("load", () => {
+  console.log(results);
+  for (let i = 0; i < 10; i++) {
+    if (typeof results.results[i] == "undefined") {
+      return;
+    }
+    titleData[i].textContent = results.results[i].title;
+    descriptionData[i].textContent = results.results[i].description;
+    linkData[i].href = results.results[i].url;
+  }
+});
